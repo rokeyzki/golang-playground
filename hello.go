86160bb58dfe145729de18530ee89af48b4b0956
rokeyzki <rokeyzki@gmail.com>
2018-12-29 18:21:33 +0800

    add feat

--- hello.go
+++ hello.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+var a = "菜鸟教程"
+var c bool
+
 func main() {
 	fmt.Println("Hello, go!")
+
+	// 这种不带声明格式的只能在函数体中出现
+	b := "runoob.com"
+	d, e, f := 1, 2, 3
+
+	println(a, b, c, d, e, f)
 }
